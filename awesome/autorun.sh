177841779777f91ab2fcf323fa49a8829a5c991f
ProgrammingHider <mrnebelfugiyt@gmail.com>
2019-10-04 20:34:21 +0200

    awesome config

---
+++ awesome/autorun.sh
@@ -0,0 +1,18 @@
+# Important
+
+#!/usr/bin/env bash
+
+function run {
+  if ! pgrep -f $1 ;
+  then
+    $@&
+  fi
+}
+
+# Fun
+
+run "nitrogen --restore"
+#run "variety"
+run "nm-applet"
+#run "cbatticon"
+run "compton -b"
